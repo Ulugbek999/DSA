d2bd1237a42400c70da9473503ff982bfbd68b00
Sports-Media-Interns <nft@mysportsmedia.com>
2025-08-31 21:16:22 -0400

    second assignment completed

--- assignment2/Main.java
+++ assignment2/Main.java
@@ -1,10 +1,15 @@
 
 
 
-public class Main{
+public class Main {
 
+    
 
-    public static void main(String args[]){
+    public static void main(String[] args) {
+
         
+        TwoSeven.solution();
+
+
     }
 }
